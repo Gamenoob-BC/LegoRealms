6259557021309de4d75b4c6295c83e2be729d7d5
Gamenoob-BC <143728735+Gamenoob-BC@users.noreply.github.com>
2023-09-07 17:28:40 +0200

    Updates

---
+++ public/client.js
@@ -0,0 +1,11 @@
+const socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server');
+});
+
+// Your game logic with THREE.js goes here
